2b1467af64fd8114620f0c882a6ecf8ca418986f
andressalessa <lessadasilveiraandressa@gmail.com>
2022-10-24 20:28:15 -0300

    Add files via upload

--- js/Form.js
+++ js/Form.js
@@ -1,6 +1,6 @@
 class Form {
   constructor() {
-    this.entrada = createInput("").attribute("placeholder", "Digite seu nome");
+    this.nome = createInput("").attribute("placeholder", "Digite seu nome");
     this.botaoJogar = createButton("Jogar");
     this.imagemTitulo = createImg("./assets/TITULO.png", "nome do jogo");
     this.mensagem = createElement("h2");
@@ -10,19 +10,46 @@ class Form {
   {
     this.mensagem.esconder();
     this.botaoJogar.esconder();
-    this.input.esconder();
+    this.nome.esconder();
   }
 
-
+  mostrar()
+  {
+    this.addPosicaoElementos();
+    this.addEstiloElementos();
+    this.gerenciarClickBotao();
+  }
 
   addPosicaoElementos()
   {
     this.imagemTitulo.position(120, 50);
-    this.entrada.position(width / 2 - 110, height / 2 - 80);
+    this.nome.position(width / 2 - 110, height / 2 - 80);
     this.botaoJogar.position(width / 2 - 90, height / 2 - 20);
     this.mensagem.position(width / 2 - 300, height / 2 - 100);
   }
 
-  
+  addEstiloElementos()
+  {
+    this.imagemTitulo.class("gameTitle");
+    this.nome.class("customInput");
+    this.botaoJogar.class("customButton");
+    this.mensagem.class("greeting");
+  }
+
+  gerenciarClickBotao() {
+    this.botaoJogar.mousePressed(() => {
+      this.nome.hide(); // hide = esconder
+      this.botaoJogar.hide(); // hide = esconder
+      
+      contadorJogadores = contadorJogadores + 1;
+      jogador.nome = this.nome.value();
+      jogador.indice = contadorJogadores;
+      jogador.addJogador();
+      jogador.atualizarNumeroJogadores(contadorJogadores);
+      
+      var mensagem = `Olah, ${this.nome.value()} </br>espere o outro jogador entrar...`;
+      this.mensagem.html(mensagem);
+    });
+  }
 
 }
